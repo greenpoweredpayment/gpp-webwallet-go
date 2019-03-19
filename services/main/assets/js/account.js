b3577b5bb1bd11627a0c4c5f860590d859cc9a50
MrGreenPower <greenpoweredpayment@gmail.com>
2019-03-19 01:30:11 +0100

    GPP_update

--- services/main/assets/js/account.js
+++ services/main/assets/js/account.js
@@ -1,6 +1,6 @@
 // Forking config.
-const tickerSymbol = 'TRTL';
-const decimalPlaces = 2;
+const tickerSymbol = 'GPP';
+const decimalPlaces = 3;
 
 // Wallet update interval in milliseconds. Probably don't need to change this.
 const updateInterval = 5000;
