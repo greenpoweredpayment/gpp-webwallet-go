f1ee09b94a126c42f0e210bc1743a970be789db9
MrGreenPower <greenpoweredpayment@gmail.com>
2019-03-24 02:47:17 +0100

    run.sh update

--- services/main/run.sh
+++ services/main/run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-HOST_URI='45.63.114.182' \
-HOST_PORT=':8080' \
+HOST_URI='104.238.167.79' \
+HOST_PORT=':80' \
 USER_URI='http://localhost:8081' \
 WALLET_URI='http://localhost:8082' \
 go run main.go utils.go
--- services/wallet/run.sh
+++ services/wallet/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-DB_USER= \
-DB_PWD= \
-HOST_URI='http://localhost' \
+DB_USER=postgres \
+DB_PWD=password \
+HOST_URI='104.238.167.79' \
 HOST_PORT=':8082' \
-RPC_PWD=  \
-RPC_PORT='8070' \
+RPC_PWD=password  \
+RPC_PORT='21315' \
 go run wallet.go utils.go
