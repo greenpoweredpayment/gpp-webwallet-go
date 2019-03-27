1b25a2211f906693d9ae11dfdd92b4f98b8e726a
MrGreenPower <greenpoweredpayment@gmail.com>
2019-03-27 01:34:35 +0100

    Update run.sh

--- services/wallet/run.sh
+++ services/wallet/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-DB_USER="postgres"
-DB_PWD="password"
+DB_USER=postgres
+DB_PWD=Losentillvultr
 HOST_URI='http://localhost'
 HOST_PORT=':8082'
 RPC_PWD=password
-RPC_PORT='8070'
+RPC_PORT='8070' \ #  21315 deamon RPC port
 go run wallet.go utils.go
